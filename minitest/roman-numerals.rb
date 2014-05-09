3220726bb4d566cdaa19b0fd50b740da85e1b1d4
Jens Jakob Jensen <jjj@jayway.com>
2014-05-09 14:57:26 +0200

    Fix and start on new design :)

--- minitest/roman-numerals.rb
+++ minitest/roman-numerals.rb
@@ -2,15 +2,41 @@ class RomanNumerals
   ROMAN = {:one => 'I',
            :five => 'V',
            :ten => 'X',
-           :hundred =>'C'}
+           :fifty => 'L',
+           :hundred =>'C',
+           :fivehundred =>'D',
+           :thousand =>'M',
+           }
+
+  tens = {1000 => :thousand,
+          500 => :fivehundred,
+          400 => [:fivehundred],
+          }
 
   def self.decimal_to_roman number
     result = ''
-    number.times {|index| result += ROMAN[:one] } if number <= 3
-    result = ROMAN[:one] + ROMAN[:five] if number == 4
-    if number % 10 == 9
-      result += ROMAN[:one] + ROMAN[:ten]
+    
+    # Tens and up...
+    while number >= 10
+      if number >= 1000
+        result = ROMAN[:thousand]+result 
+        number -= 1000
+      end
+      if number >= 500
+        result = ROMAN[:fivehundred]+result 
+        number -= 500
+      end
+      if number >= 400
+        result = ROMAN[:hundred]+ROMAN[:fivehundred]+result 
+        number -= 400
+      end
     end
+    
+    # Ones
+    number.times {|index| result += ROMAN[:one] } if number.between? 0,3
+    result = ROMAN[:one] + ROMAN[:five] if number == 4
+    result = ROMAN[:five] + ROMAN[:one] if number == 6
+    result = ROMAN[:one] + ROMAN[:ten] if number == 9
     result
   end
 end
