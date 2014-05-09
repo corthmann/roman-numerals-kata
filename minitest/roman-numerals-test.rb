2dcc6a3ff439f106782fff367577518c795fea18
Jens Jakob Jensen <jjj@jayway.com>
2014-05-09 15:18:11 +0200

    Add test for first principle from http://www.factmonster.com/ipka/A0769547.html
    1. A letter repeats its value that many times (XXX = 30, CC = 200, etc.). A letter can only be repeated three times.

--- minitest/roman-numerals-test.rb
+++ minitest/roman-numerals-test.rb
@@ -23,4 +23,18 @@ describe 'RomanNumerals' do
     roman = RomanNumerals.decimal_to_roman(9)
     assert_equal('IX', roman)
   end
+
+  it 'A letter repeats its value that many times (XXX = 30, CC = 200, etc.). A letter can only be repeated three times.' do
+    # Test first part of spec:
+    assert_equal('XXX', RomanNumerals.decimal_to_roman(30))
+    assert_equal('CC', RomanNumerals.decimal_to_roman(200))
+    assert_equal('MMMCCCXXXIII', RomanNumerals.decimal_to_roman(3333))
+    assert_equal('MMCCXXII', RomanNumerals.decimal_to_roman(2222))
+    assert_equal('MCXI', RomanNumerals.decimal_to_roman(1111))
+    # Test last part of spec:
+    assert_equal('IV', RomanNumerals.decimal_to_roman(4))
+    # TODO (currently non-terminating)
+    #assert_equal('XL', RomanNumerals.decimal_to_roman(40))
+    #assert_equal('CM', RomanNumerals.decimal_to_roman(400))
+  end
 end
