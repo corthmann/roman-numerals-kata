03c663a776e80fac65f6c6eb46a4e3f27bd5971c
Jens Jakob Jensen <jjj@jayway.com>
2014-05-09 16:01:42 +0200

    Final implementation :-)

--- minitest/roman-numerals.rb
+++ minitest/roman-numerals.rb
@@ -1,42 +1,30 @@
 class RomanNumerals
-  ROMAN = {:one => 'I',
-           :five => 'V',
-           :ten => 'X',
-           :fifty => 'L',
-           :hundred =>'C',
-           :fivehundred =>'D',
-           :thousand =>'M',
-           }
-
-  tens = {1000 => :thousand,
-          500 => :fivehundred,
-          400 => [:fivehundred],
-          }
 
   def self.decimal_to_roman number
     result = ''
     
-    # Tens and up...
-    while number >= 10
-      if number >= 1000
-        result = ROMAN[:thousand]+result 
-        number -= 1000
-      end
-      if number >= 500
-        result = ROMAN[:fivehundred]+result 
-        number -= 500
-      end
-      if number >= 400
-        result = ROMAN[:hundred]+ROMAN[:fivehundred]+result 
-        number -= 400
-      end
+    ones = ['I','II','III','IV','V','VI','VII','VIII','IX']
+    
+    tens      = ones.map { |str| str.gsub('V','L').gsub('I','X') }
+    hundreds  = ones.map { |str| str.gsub('V','D').gsub('I','C') }
+    thousands = ones.map { |str| str.gsub('V',' ').gsub('I','M') }
+    
+    if (1000..4000) === number
+      result = result + thousands[number/1000 - 1]
+      number = number % 1000
+    end
+    if (100..999) === number
+      result = result + hundreds[number/100 - 1]
+      number = number % 100
+    end
+    if (10..99) === number
+      result = result + tens[number/10 - 1]
+      number = number % 10
+    end
+    if (1..9) === number
+      result = result + ones[number-1]
     end
     
-    # Ones
-    number.times {|index| result += ROMAN[:one] } if number.between? 0,3
-    result = ROMAN[:one] + ROMAN[:five] if number == 4
-    result = ROMAN[:five] + ROMAN[:one] if number == 6
-    result = ROMAN[:one] + ROMAN[:ten] if number == 9
     result
   end
 end
